e63aae8699b91c23c82443a872151f694fd3735f
ZHANGKAIHENG <120181080602@ncepu.edu.cn>
2020-04-08 10:31:40 +0800

    practice on class

---
+++ class_4_8_demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+"""
+@File : class_4_8_demo.py
+@Time : 2020/04/08 10:28:10
+@Author : KingFar 
+@Version : 1.0
+@Contact : 1136421682@qq.com
+@WebSite : https://github.com/KingFarGrace
+"""
+
+# here put the import lib
+
+"""
+"""
