371a0ecd26fa15899eeaffa897d12f1fb80e0f7a
ZHANGKAIHENG <120181080602@ncepu.edu.cn>
2020-04-23 01:26:50 +0800

    homework7

--- homework7/task1.py
+++ homework7/task1.py
@@ -11,8 +11,21 @@
 """
 
 # here put the import lib
+import re
 
 """
 给定一个文件 webspiderUrl.txt，请用正则表达式，逐行匹配提取其中的URL链接信息，并保存到另外一个文件中；
 提示，文件有1000行，注意控制每次读取的行数
 """
+
+if __name__ == '__main__':
+    # 因为文件中只有网址是http://开头的格式，因此后续只需要匹配可能出现的字符即可
+    rule = re.compile(r"http://[a-zA-Z0-9\.\-\_]{0,25}")
+    with open(r"homework7\webspiderUrl.txt", "r", encoding="utf-8") as rf:
+        with open(r"homework7\url.txt", "w") as wf:
+            for line in rf:
+                all_match = rule.findall(line)
+                for rec in all_match:
+                    wf.write(rec)
+                    wf.write('\n')
+    print("All done.")
